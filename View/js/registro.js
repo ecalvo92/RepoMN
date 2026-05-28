511629c8d46dd04b433035efa6f57780d5254657
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-05-27 19:51:52 -0600

    Se inactiva y cambia de color a una caja de texto

---
+++ View/js/registro.js
@@ -0,0 +1,15 @@
+$(function () {
+
+    $("#nombre").prop("readonly", true);
+    $("#nombre").css("background-color", "#d9dde28d");
+
+});
+
+/*
+    document.addEventListener("DOMContentLoaded", function () {
+        
+        document.getElementById("nombre").readOnly = true;
+        document.getElementById("nombre").style.backgroundColor = "#d9dde28d";
+
+    });
+*/
