e4fad8237253e93dffa8cc9e37805bfa043e2cc3
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-06-03 18:34:23 -0600

    Organización de vistas y archivos .php

--- View/vInicio/RegistrarUsuarios.php
+++ View/vInicio/RegistrarUsuarios.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <title>Proyecto Web MN</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="../css/main.css" />
 </head>
 
 <body>
@@ -13,23 +13,30 @@
         <div class="card" style="max-width: 420px; width: 100%">
             <div class="card-body p-5">
                 <div class="text-center mb-3">
-                    <a href="index.html" class="mb-2 d-inline-block"><img src="images/logo-fidelitas.png" alt=""
+                    <a href="index.html" class="mb-2 d-inline-block"><img src="../images/logo-fidelitas.png" alt=""
                             width="130" />
                     </a>
-                    <h1 class="card-title mb-5 h5">Iniciar Sesión</h1>
+                    <h1 class="card-title mb-5 h5">Registrar Usuarios</h1>
                 </div>
 
                 <form class="needs-validation mt-3" novalidate>
                     <div class="mb-3">
                         <label for="identificacion" class="form-label">Identificación</label>
-                        <input id="identificacion" type="text" class="form-control" required autofocus />
+                        <input id="identificacion" type="text" class="form-control" required />
                     </div>
 
                     <div class="mb-3">
-                        <label for="contrasenna" class="form-label d-flex justify-content-between">
-                            <span>Contraseña</span>
-                            <a href="RecuperarAcceso.html" class="small link-primary">¿Olvidó su contraseña?</a>
-                        </label>
+                        <label for="nombre" class="form-label">Nombre</label>
+                        <input id="nombre" type="text" class="form-control" required />
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="correoElectronico" class="form-label">Correo Electrónico</label>
+                        <input id="correoElectronico" type="text" class="form-control" required />
+                    </div>
+
+                    <div class="mb-3">
+                        <label for="contrasenna" class="form-label">Contraseña</label>
                         <input id="contrasenna" type="password" class="form-control" required minlength="6" />
                     </div>
 
@@ -37,13 +44,16 @@
                 </form>
 
                 <div class="text-center mt-3 small text-muted">
-                    ¿No tiene una cuenta?
-                    <a href="RegistrarUsuarios.html" class="link-primary">Regístrese</a>
+                    ¿Ya tiene una cuenta?
+                    <a href="IniciarSesion.php" class="link-primary">Inicie Sesión</a>
                 </div>
             </div>
         </div>
     </div>
 
     <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="../js/registro.js"></script>
 </body>
+
 </html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+    header("Location: View/vInicio/IniciarSesion.php");
+    exit();
