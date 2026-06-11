55f03c238f9c8a475b7a8b24bce331987ab6d657
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-06-10 18:53:07 -0600

    Comunicación entre V - C - M -> BD

--- Controller/InicioController.php
+++ Controller/InicioController.php
@@ -1,6 +1,13 @@
 <?php
+    include_once '../../Model/InicioModel.php';
 
-if(isset($_POST["btnRegistrar"]))
-{
-    echo $_POST["identificacion"];
-}
+    if(isset($_POST["btnRegistrar"]))
+    {
+        $identificacion = $_POST["identificacion"];
+        $nombre = $_POST["nombre"];
+        $correoElectronico = $_POST["correoElectronico"];
+        $contrasenna = $_POST["contrasenna"];
+
+        RegistrarUsuarioModel($identificacion,$nombre,$correoElectronico,$contrasenna);
+
+    }
---
+++ Model/InicioModel.php
@@ -0,0 +1,6 @@
+<?php
+
+    function RegistrarUsuarioModel($identificacion,$nombre,$correoElectronico,$contrasenna)
+    {
+        
+    }
