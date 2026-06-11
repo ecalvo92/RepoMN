b56118cdaefa3c3801d6411a0ab909869ea37d2f
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-06-10 19:30:10 -0600

    Update InicioModel.php

--- Model/InicioModel.php
+++ Model/InicioModel.php
@@ -2,5 +2,21 @@
 
     function RegistrarUsuarioModel($identificacion,$nombre,$correoElectronico,$contrasenna)
     {
-        
+        $conn = new mysqli("127.0.0.1:3307", "root", "", "mn");
+
+        $sql = "INSERT INTO tb_usuario
+                (Identificacion,
+                Nombre,
+                CorreoElectronico,
+                Contrasenna,
+                Estado)
+                VALUES
+                ('$identificacion',
+                 '$nombre',
+                 '$correoElectronico',
+                 '$contrasenna',
+                 1);";
+
+        $conn -> query($sql);
+        $conn -> close();
     }
