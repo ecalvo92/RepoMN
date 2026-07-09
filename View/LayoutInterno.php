f7f7cec73d7af7fba282192a9991672128627db3
Eduardo Calvo Castillo <ecalvo@crnova.com>
2026-07-08 20:52:02 -0600

    Estilo, validaciones y el diseño

--- View/LayoutInterno.php
+++ View/LayoutInterno.php
@@ -24,6 +24,8 @@ function ImportJS()
     echo '
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="../js/sidebar.js"></script>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.21.0/dist/jquery.validate.min.js"></script>
     ';
 }
 
